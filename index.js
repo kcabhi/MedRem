3660c4890d29c0a9cbb1df5489f296e4d544adfa
kcabhi <07kcav@gmail.com>
2019-05-02 08:26:56 +0545

    application server initilized

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Index is running");
+//var application = require
+var express = require("express");
+
+var application1 = new express();
+
+application1.listen(3001);
+
+var application2 = new express();
+application2.listen(3002);
